a75b4ba42de49570caae05fc3e09864226a2c40c
Javier Sivianes <sivious@gmail.com>
2026-04-14 17:03:23 +0200

    Initial commit

---
+++ src/main/java/com/tests/testjava2026/TestJava2026Application.java
@@ -0,0 +1,13 @@
+package com.tests.testjava2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJava2026Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJava2026Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/tests/testjava2026/TestJava2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tests.testjava2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJava2026ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
